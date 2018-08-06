7148ed7b493dc95b35e8d8212f9b50645364d2bf
Jon Massey <jm4359@bristol.ac.uk>
2018-08-06 21:18:43 +0100

    Added SimMetrics.NET references, skeleton metric, name-cleaning logic

---
+++ MedicineMetric.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SimMetrics.Net.API;
+
+namespace VetMedData.NET
+{
+    class MedicineMetric :AbstractStringMetric
+    {
+        public override double GetSimilarity(string firstWord, string secondWord)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetSimilarityExplained(string firstWord, string secondWord)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override double GetSimilarityTimingEstimated(string firstWord, string secondWord)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override double GetUnnormalisedSimilarity(string firstWord, string secondWord)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string LongDescriptionString { get; }
+        public override string ShortDescriptionString { get; }
+    }
+}
---
+++ ProductExtensions.cs
@@ -0,0 +1,55 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace VetMedData.NET
+{
+    /// <summary>
+    /// Extension methods for Product class
+    /// </summary>
+    public static class ProductExtensions
+    {
+        private const string BracketedTermPattern = @"[\[\(] *\w* *[\]\)]";
+        private const string LeadingMultiplierPattern = @"^[0-9]+ *[xX] *(?=\w*)";
+
+        /// <summary>
+        /// Extension method to provide "cleaned" version of name according
+        /// to rules defined in configuration class
+        /// </summary>
+        /// <param name="product">Product from which cleaned name will be extracted</param>
+        /// <param name="cfg">Configuration for cleaning rules</param>
+        /// <returns></returns>
+        public static string GetCleanedName(this Product product, NameCleaningConfig cfg)
+        {
+            var outstr = product.Name;
+
+            outstr = cfg.RemoveBracketedTerms ?
+                Regex.Replace(outstr, BracketedTermPattern, "",
+                    RegexOptions.Compiled) :
+                outstr;
+
+            outstr = cfg.RemoveLeadingMultiplierTerms ? 
+                Regex.Replace(outstr,LeadingMultiplierPattern,"",
+                    RegexOptions.Compiled) : 
+                outstr;
+
+            outstr = cfg.PatternsToRemove != null && cfg.PatternsToRemove.Length > 0 ?
+                cfg.PatternsToRemove.Aggregate(outstr, (current, pat) =>
+                    Regex.Replace(current, pat, "",
+                        RegexOptions.Compiled)) :
+                outstr;
+
+            outstr = cfg.CharsToRemove != null && cfg.CharsToRemove.Length > 0 ?
+                cfg.CharsToRemove.Aggregate(outstr, (current, rmchar) =>
+                     current.Replace(rmchar.ToString(), "")) :
+                outstr;
+
+            outstr = cfg.TermsToRemove != null && cfg.TermsToRemove.Length > 0 ?
+                cfg.TermsToRemove.Aggregate(outstr, (current, rmstr) =>
+                     Regex.Replace(current, $"\\b{rmstr}\\b", "",
+                     RegexOptions.IgnoreCase & RegexOptions.Compiled)) :
+                outstr;
+
+            return outstr;
+        }
+    }
+}
