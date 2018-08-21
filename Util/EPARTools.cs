853ab15cdd641a2f5d431c26be105e357ec9ae95
Jon Massey <jm4359@bristol.ac.uk>
2018-08-21 17:31:54 +0100

    EPARTools and constructor

---
+++ Util/EPARTools.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace VetMedData.NET.Util
+{
+    // ReSharper disable once InconsistentNaming
+    public class EPARTools
+    {
+        private static EPARTools _eparTools;
+        private static string _googleCustomSearchAPIKey;
+
+        public EPARTools(IConfiguration conf)
+        {
+            _googleCustomSearchAPIKey = conf["GoogleAPISecrets:GoogleCustomSearchAPIKey"];
+        }
+        public static EPARTools Get()
+        {
+            var builder = new ConfigurationBuilder()
+                .AddUserSecrets("3ed32012-d532-4d6f-9a0a-9a5ba6c7c636");
+
+            _eparTools = _eparTools ?? new EPARTools(builder.Build());
+            return _eparTools;
+        }
+
+        private static bool IsEPAR(string url)
+        {
+            return url.Contains(@"ema.europa.eu/ema");
+        }
+
+    }
+
+}
