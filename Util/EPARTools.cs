132f7940b79a9bf7cbe11c72e27bfb6720d124ff
Jon Massey <jm4359@bristol.ac.uk>
2018-08-22 14:36:14 +0100

    Removed google custom search, added direct EMA scrape

--- Util/EPARTools.cs
+++ Util/EPARTools.cs
@@ -1,34 +1,57 @@
-﻿using Microsoft.Extensions.Configuration;
-using System.IO;
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace VetMedData.NET.Util
 {
     // ReSharper disable once InconsistentNaming
     public class EPARTools
     {
-        private static EPARTools _eparTools;
-        private static string _googleCustomSearchAPIKey;
-        private static string _googleCustomSearchCX;
+        private static readonly Uri EmaBaseUri = new Uri("http://www.ema.europa.eu/ema/");
 
-        public EPARTools(IConfiguration conf)
-        {
-            _googleCustomSearchAPIKey = conf["GoogleAPISecrets:GoogleCustomSearchAPIKey"];
-            _googleCustomSearchCX = conf["GoogleAPISecrets:GoogleCustomSearchCX"];
-        }
-        public static EPARTools Get()
-        {
-            var builder = new ConfigurationBuilder()
-                .AddUserSecrets("3ed32012-d532-4d6f-9a0a-9a5ba6c7c636");
-
-            _eparTools = _eparTools ?? new EPARTools(builder.Build());
-            return _eparTools;
-        }
+        private const string EmaSearchUrl =
+            @"index.jsp?curl=pages%2Fmedicines%2Flanding%2Fvet_epar_search.jsp&mid=WC0b01ac058001fa1c&searchTab=searchByKey&alreadyLoaded=true&isNewQuery=true&status=Authorised&status=Withdrawn&status=Suspended&status=Refused&keyword={prodname}&keywordSearch=Submit&searchType=name&taxonomyPath=&treeNumber=";
 
         private static bool IsEPAR(string url)
         {
             return url.Contains(@"ema.europa.eu/ema");
         }
 
-    }
+        public static async Task<string[]> GetSearchResults(string productName)
+        {
+            using (var cli = new HttpClient())
+            {
+                cli.BaseAddress = EmaBaseUri;
+                var innerlinks = new List<string>();
+                var res = await cli.GetAsync(EmaSearchUrl.Replace("{prodname}", productName));
 
+                var doc = new HtmlDocument();
+                doc.Load(await res.Content.ReadAsStreamAsync());
+
+                var outerlinks = doc.DocumentNode.SelectNodes("//a[@href]");
+                foreach (var outerlink in outerlinks.Where(n =>
+                    n.InnerText.Equals(productName, StringComparison.InvariantCultureIgnoreCase)))
+                {
+                    var innerres = await cli.GetAsync(outerlink.Attributes["href"].Value);
+
+                    var innerdoc = new HtmlDocument();
+                    innerdoc.Load(await innerres.Content.ReadAsStreamAsync());
+                    var doclinks = innerdoc.DocumentNode.SelectNodes("//a[@href]")
+                        .Where(n =>
+                            n.InnerText.Contains("EPAR - Product Information") &&
+                            n.Attributes["href"].Value.Contains("en_GB") &&
+                            n.Attributes["href"].Value.EndsWith(".pdf"))
+                        .Select(n => $"http://{EmaBaseUri.Host}{n.Attributes["href"].Value}");
+
+                    innerlinks.AddRange(doclinks);
+                }
+
+                return innerlinks.ToArray();
+            }
+
+        }
+    }
 }
